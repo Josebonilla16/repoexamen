33480c257c81edd299c98f804a7c457ef31b9917
Josebonilla16 <Josephman1253@hotmail.com.com>
2018-11-19 11:36:28 -0600

    cre el modelo y las urls

--- materias/admin.py
+++ materias/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from materias.models import  Materia, MateriaAdmin, Grado, GradoAdmin
 # Register your models here.
+admin.site.register(Materia, MateriaAdmin)
+admin.site.register(Grado, GradoAdmin)
--- materias/models.py
+++ materias/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib import admin
 
-# Create your models here.
+class Materia(models.Model):
+    nombre  =   models.CharField(max_length=200)
+    def __str__(self):
+        return self.nombre
+
+class Grado(models.Model):
+    grado  =   models.CharField(max_length=200)
+    seccion =   models.CharField(max_length=200)
+    materias   = models.ManyToManyField(Materia, through='Detalle')
+    def __str__(self):
+        return self.grado
+
+class Detalle (models.Model):
+    materia = models.ForeignKey(Materia, on_delete=models.CASCADE)
+    grado = models.ForeignKey(Grado, on_delete=models.CASCADE)
+
+class DetalleInLine(admin.TabularInline):
+    model = Detalle
+    extra = 1
+
+class MateriaAdmin(admin.ModelAdmin):
+    inlines = (DetalleInLine,)
+
+class GradoAdmin (admin.ModelAdmin):
+    inlines = (DetalleInLine,)
