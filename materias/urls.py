6f28ae58472e64c5f64fb574df33ce3cc1341267
Josebonilla16 <Josephman1253@hotmail.com.com>
2018-11-19 12:25:05 -0600

    termine creando el modelo template vista y url

--- materias/urls.py
+++ materias/urls.py
@@ -2,6 +2,5 @@ from django.conf.urls import url, include
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.lista_peliculas, name ='lista_peliculas'),
-    url(r'^pelicula/nueva/$', views.pelicula_nueva, name='pelicula_nueva'),
+    url(r'^materias/nuevo/$', views.materia_nuevo, name='materia_nuevo'),
     ]
--- materias/views.py
+++ materias/views.py
@@ -1,3 +1,18 @@
+from django.contrib import messages
+from .forms import GradoForm
 from django.shortcuts import render
+from django.contrib import messages
+from materias.models import Grado, Materia, Detalle
 
-# Create your views here.
+def materia_nuevo (request):
+    if request.method == "POST":
+        formulario = GradoForm(request.POST)
+        if formulario.is_valid():
+            grado = Grado.objects.create(grado=formulario.cleaned_data['grado'], seccion=formulario.cleaned_data['seccion'])
+            for materia_id in request.POST.getlist('materias'):
+                detalle = Detalle(materia_id=materia_id, grado_id = grado.id)
+                detalle.save()
+            messages.add_message(request, messages.SUCCESS, 'Materias asignadas exitosamente')
+    else:
+        formulario = GradoForm()
+    return render(request, 'materias/materia_editar.html', {'formulario': formulario})
